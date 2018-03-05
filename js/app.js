77d3d77a60b1025acebea38ab03c785351d38fba
Alec Wilbur <lawilbur2@gmail.com>
2018-03-05 14:15:22 -0500

    cycle through hands and create crib

--- js/app.js
+++ js/app.js
@@ -1,91 +1,140 @@
+let deck = [];
+let crib = [];
+let player1hand = [];
+let player1Score = 0;
+let player2hand = [];
+let player2Score = 0;
+let inPlay = [];
+let turn = true;
+let isEndOfTurn = false;
+const suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs'];
+class Card {
+    constructor(suit, value, title) {
+        this.suit = suit;
+        this.value = value;
+        this.title = title;
+    }
+};
 
+const checkPlayerturn = () =>{
+    if(turn === true){
+        return player1hand;
+    }else {
+        return player2hand;
+    }
+};
 
+const createBoard = ()=>{
+    for(let i = 1; i<122; i++){
+        const $div = $('<div>').addClass('circle');
+        const $scoreMarker = $('<div>').addClass('scoreMarker').text('-'+i+'-');
+        if(i % 5 === 0){
+            $('#scoringSection').append($div);
+            $('#scoringSection').append($scoreMarker);
+        }else {
+            $('#scoringSection').append($div);
+        }
+
+    }
+};
+
+const theCutStage = () =>{
+    const $div = $('<div>').attr('id','theCut');
+    $div.append($('<h2>').text(deck[19].suit)); // 19 equals middle of deck
+    $div.append($('<h2>').text(deck[19].title));
+    $('#deck').append($div);
+};
+
+const theCribStage = () =>{
+    displayHands();
+    console.log('getting out of display hands');
+    if(player1hand.length == 4){
+        console.log("hand length");
+    }
+};
+const endTurn = ()=>{
+    $('#playArea').empty();
+    $('#handArea').empty();
+    isEndOfTurn = false;
+    turn = !turn;
+    displayHands();
+};
+ const showEndTurnButton = () =>{
+     if(! isEndOfTurn){
+         const $button = $('<button>').text('End Turn');
+         $('#playArea').append($button);
+         $button.on('click' , (event)=>{
+             endTurn();
+         });
+         isEndOfTurn = true;
+     };
+ };
+
+const displayHands = () =>{ // should take the player as a parameter maybut stage as well
+    const currentPlayer = checkPlayerturn();
+    for(let i = 0; i<currentPlayer.length; i++){
+        const $div = $('<div>').addClass('cardDisplay');
+        $div.append($('<h2>').text(currentPlayer[i].suit));
+        $div.append($('<h2>').text(currentPlayer[i].title));
+        $('#handArea').append($div);
+        $div.on('click', (event)=>{
+            if(currentPlayer.length > 4){
+                $div.appendTo('#playArea');
+                crib.push(currentPlayer[i]);
+                currentPlayer.splice(i,1);
+                console.log(currentPlayer);
+                console.log(crib);
+
+            }
+            if(currentPlayer.length == 4){
+                showEndTurnButton();
+            }
+
+        });
+    }
+};
+
+const createHands = () =>{
+    deck.sort((a, b)=>{
+        return 0.5 - Math.random(); //randomizes the deck
+    });
+    deck.sort((a, b)=>{
+        return 0.5 - Math.random(); //randomizes the deck
+    });
+    for(i = 0; i < 12; i++){
+        if( i % 2 === 0){
+        player1hand.push(deck[i]);
+        }else {
+        player2hand.push(deck[i]);
+        }
+    }
+    deck.splice(0, 12); // removes cards delt
+
+}
+
+const createDeck = () =>{
+    for(let i = 0; i<4; i++){
+        for(let j = 1; j < 14; j++){
+            if(j <= 10){
+            deck.push(new Card(suits[i], j , j));
+        }else if (j == 11){
+            deck.push(new Card(suits[i], 10, 'Jack'));
+        }else if (j == 12){
+            deck.push(new Card(suits[i], 10, 'Queen'));
+        }else if (j == 13){
+            deck.push(new Card(suits[i], 10, 'King'));
+        }
+        }
+    }
+}
 
 $(()=>{
-    let deck = [];
-    class Card {
-        constructor(suit, value, title) {
-            this.suit = suit;
-            this.value = value;
-            this.title = title;
-        }
-    }
-
-    let crib = [];
-    let handA = [];
-    let computerhand = [];
-    let turn = true;
-    const suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs'];
-
-    const $createBoard = ()=>{
-        // const $div = $('<div>').addClass('circle');
-        for(let i = 0; i<120; i++){
-            const $div = $('<div>').addClass('circle');
-            $('#scoringSection').append($div)
-        }
-    }
-
-    const theCut = () =>{
-        console.log(deck[19].title);
-    }
-
-    const handToCrib = () =>{
-        crib.push(computerhand[0]);
-        crib.push(computerhand[1]);
-        computerhand.splice(0, 2);
-        crib.push(handA[0]);
-        crib.push(handA[1]);
-        handA.splice(0, 2);
-        // $('.cardInHand').on('click', ()=>{
-        //     $(event.currentTarget).attr('class', 'inCrib');
-        //     //move to crib array
-        // });
-    }
-
-    const createHands = () =>{
-        deck.sort((a, b)=>{
-            return 0.5 - Math.random(); //randomizes the deck
-        });
-        deck.sort((a, b)=>{
-            return 0.5 - Math.random(); //randomizes the deck
-        });
-        for(i = 0; i < 12; i++){
-            if( i % 2 === 0){
-            handA.push(deck[i]);
-            }else {
-            computerhand.push(deck[i]);
-            }
-        }
-        deck.splice(0, 12); // removes cards delt
-
-    }
-
-    const createDeck = () =>{
-        for(let i = 0; i<4; i++){
-            for(let j = 1; j < 14; j++){
-                if(j <= 10){
-                deck.push(new Card(suits[i], j , j));
-            }else if (j == 11){
-                deck.push(new Card(suits[i], 10, 'Jack'));
-            }else if (j == 12){
-                deck.push(new Card(suits[i], 10, 'Queen'));
-            }else if (j == 13){
-                deck.push(new Card(suits[i], 10, 'King'));
-            }
-            }
-        }
-    }
 
     createDeck();
-    console.log(deck);
     createHands();
-    console.log(handA);
-    handToCrib();
-    console.log(computerhand);
-    console.log(deck);
-    console.log(crib);
-    theCut();
-    $createBoard();
+    theCutStage();
+    createBoard();
+    theCribStage();
 });
 // const createDeck = () =>{
 //     for(let key in deck){
@@ -99,3 +148,14 @@ $(()=>{
 //         }
 //     }
 // }
+// const handToCrib = () =>{
+//     crib.push(player2hand[0]);
+//     crib.push(player2hand[1]);
+//     player2hand.splice(0, 2);
+//     crib.push(player1hand[0]);
+//     crib.push(player1hand[1]);
+//     player1hand.splice(0, 2);
+    // $('.cardInHand').on('click', ()=>{
+    //     $(event.currentTarget).attr('class', 'inCrib');
+    //     //move to crib array
+    // });
